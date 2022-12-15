8e149ee3d9e984cfa1a476d3ccd8e8df37942fb8
Terrill Hilliard <119083888+terrillhilliard@users.noreply.github.com>
2022-12-15 11:07:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".drum")[0].addEventListener("click", function() {
-  var audio = new Audio("sounds/crash.mp3");
+  var audio = new Audio("crash.mp3");
   audio.play();
 });
 
