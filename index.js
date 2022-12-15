26b755e159761464cd9f5fa3c1e5bc83f53272a3
Terrill Hilliard <119083888+terrillhilliard@users.noreply.github.com>
2022-12-15 08:48:52 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+document.querySelectorAll(".drum")[0].addEventListener("click", function() {
+  var audio = new Audio("sounds/crash.mp3");
+  audio.play();
+});
+
+document.querySelectorAll(".drum")[1].addEventListener("click", function() {
+  var audio = new Audio("kick-bass.mp3");
+  audio.play();
+});
+
+document.querySelectorAll(".drum")[2].addEventListener("click", function() {
+  var audio = new Audio("snare.mp3");
+  audio.play();
+});
+
+document.querySelectorAll(".drum")[3].addEventListener("click", function() {
+  var audio = new Audio("tom-1.mp3");
+  audio.play();
+});
+
+document.querySelectorAll(".drum")[4].addEventListener("click", function() {
+  var audio = new Audio("tom-2.mp3");
+  audio.play();
+});
+
+document.querySelectorAll(".drum")[5].addEventListener("click", function() {
+  var audio = new Audio("tom-3.mp3");
+  audio.play();
+});
+
+document.querySelectorAll(".drum")[6].addEventListener("click", function() {
+  var audio = new Audio("tom-4.mp3");
+  audio.play();
+});
