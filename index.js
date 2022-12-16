1e4c8f73558f1b36d4df861eb2d0ea7c6bbcd00f
Terrill Hilliard <119083888+terrillhilliard@users.noreply.github.com>
2022-12-16 11:27:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
-document.querySelectorAll(".drum")[0].addEventListener("click", function() {
-  var audio = new Audio("crash.mp3");
-  audio.play();
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+  });
+
+}
+
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
 });
 
-document.querySelectorAll(".drum")[1].addEventListener("click", function() {
-  var audio = new Audio("kick-bass.mp3");
-  audio.play();
-});
 
-document.querySelectorAll(".drum")[2].addEventListener("click", function() {
-  var audio = new Audio("snare.mp3");
-  audio.play();
-});
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("crash.mp3");
+      crash.play();
+      break;
 
-document.querySelectorAll(".drum")[3].addEventListener("click", function() {
-  var audio = new Audio("tom-1.mp3");
-  audio.play();
-});
+    case "a":
+      var kick = new Audio("kick-bass.mp3");
+      kick.play();
+      break;
 
-document.querySelectorAll(".drum")[4].addEventListener("click", function() {
-  var audio = new Audio("tom-2.mp3");
-  audio.play();
-});
+    case "s":
+      var snare = new Audio("snare.mp3");
+      snare.play();
+      break;
 
-document.querySelectorAll(".drum")[5].addEventListener("click", function() {
-  var audio = new Audio("tom-3.mp3");
-  audio.play();
-});
+    case "d":
+      var tom1 = new Audio("tom-1.mp3");
+      tom1.play();
+      break;
 
-document.querySelectorAll(".drum")[6].addEventListener("click", function() {
-  var audio = new Audio("tom-4.mp3");
-  audio.play();
-});
+    case "j":
+      var tom2 = new Audio("tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+
+  }
+}
